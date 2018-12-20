89eed494f6543a63b44798b67447bcced30bc13a
Kevin Haiduy Phung <kphung29@gmail.com>
2018-12-19 23:55:43 -0500

    updated to airbnb styling

--- server/index.js
+++ server/index.js
@@ -2,16 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 
-let app = express();
+const app = express();
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-let port = 3000;
+const port = 3000;
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
-
