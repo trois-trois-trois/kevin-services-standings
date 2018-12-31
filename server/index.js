90f746d30aa327de0eccf0a5cb29e39e71fc021f
Kevin Haiduy Phung <kphung29@gmail.com>
2018-12-30 19:11:45 -0500

    fetching seed data from database successfully

--- server/index.js
+++ server/index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 
-
 const app = express();
 
 app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-console.log(`this is db: ${db}`);
+
+app.get('/espn/find', (req, res) => {
+  db.find({}, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 const port = 3000;
 
