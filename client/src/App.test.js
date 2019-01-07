e313ba4d3e5eeee9dd3868806f2657a2a2c154ae
Kevin Haiduy Phung <kphung29@gmail.com>
2019-01-06 23:54:23 -0500

    component tests passing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,14 +1,39 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable no-undef */
 import App from './App';
+import Standings from './components/Standings';
 
 describe('App component', () => {
-  it('renders correctly', () => {
+  it('Should render App component correctly', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
-});
 
-it('adds correctly', () => {
-  expect(1 + 1).toEqual(2);
+  it('Should update a state in a callback', () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ scores: 'new' }, () => {
+      expect(wrapper.state()).toEqual({ scores: 'new', teams: [], view: 'main' });
+    });
+  });
+
+  test('Should render a small label', () => {
+    const wrapper = shallow(
+      <App small>This is a test!</App>,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Should call componentDidMount once', () => {
+    const spy = jest.spyOn(App.prototype, 'componentDidMount');
+    const wrapper = mount(<App />);
+    wrapper.instance().componentDidMount();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('Should update state view when handleClick() is called', () => {
+    const wrapper = shallow(<App />);
+    wrapper.instance().handleClick();
+    expect(wrapper.state()).toEqual({ teams: [], view: 'fullstandings' });
+  });
 });
