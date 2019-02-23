b8f458f65397be0234089c358b83e505459efd02
Faris Habib <faris.habib.7@gmail.com>
2019-02-22 21:45:16 -0800

    build: add environment settings for ec2 deployment

--- database/config.js
+++ database/config.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'ec2-3-16-125-146.us-east-2.compute.amazonaws.com',
     user: 'fluffy',
     password: 'troiscubed',
-    database: 'espn',
+    database: 'espndev',
   },
 });
 
